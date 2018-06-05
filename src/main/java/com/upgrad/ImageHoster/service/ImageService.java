685a8df355a36946d86772a5588a32a306337d4d
Gagan Raj M <gagan.rajm@ellucian.com>
2018-06-05 23:11:06 +0530

    Modified methods to take id of a image instead of title.

--- src/main/java/com/upgrad/ImageHoster/service/ImageService.java
+++ src/main/java/com/upgrad/ImageHoster/service/ImageService.java
@@ -7,8 +7,10 @@ import java.util.List;
 public interface ImageService{
     List<Image> getAll();
     List<Image> getByTag(String tagName);
-    Image getByTitle(String title);
-    Image getByTitleWithJoin(String title);
+    // changed method name and input param to return image specific to image id
+    Image getById(int id);
+    // changed method name and input param to return image specific to image id
+    Image getByIdWithJoin(int id);
     void deleteByTitle(Image image);
     void save(Image image);
     void update(Image image);
--- src/main/java/com/upgrad/ImageHoster/service/ImageServiceImpl.java
+++ src/main/java/com/upgrad/ImageHoster/service/ImageServiceImpl.java
@@ -21,12 +21,12 @@ public class ImageServiceImpl implements ImageService {
     public List<Image> getByTag(String tagName) { return imageManager.getImagesByTag(tagName); }
 
     @Override
-    public Image getByTitle(String title) {
-       return imageManager.getImageByTitle(title);
+    public Image getById(int id) {
+       return imageManager.getImageById(id);
     }
 
     @Override
-    public Image getByTitleWithJoin(String title) { return imageManager.getImageByTitleWithJoins(title); }
+    public Image getByIdWithJoin(int id) { return imageManager.getImageByIdWithJoins(id); }
 
     @Override
     public void deleteByTitle(Image image) {
